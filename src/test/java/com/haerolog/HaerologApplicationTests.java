33e6c4c427217cc7b508d127a9aa2c0540b74391
Seonho Kim <sunhowold@naver.com>
2023-06-28 12:09:54 +0900

    initial commit

---
+++ src/test/java/com/haerolog/HaerologApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haerolog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaerologApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
