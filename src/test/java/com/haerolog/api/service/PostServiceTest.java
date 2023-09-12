76a563287e695364cf517e6512a261b1263c080a
haero77 <shineecard5@gmail.com>
2023-09-12 23:32:24 +0900

    test: Post 서비스 테스트 추가

---
+++ src/test/java/com/haerolog/api/service/PostServiceTest.java
@@ -0,0 +1,49 @@
+package com.haerolog.api.service;
+
+import com.haerolog.api.domain.Post;
+import com.haerolog.api.repository.PostRepository;
+import com.haerolog.api.request.PostCreate;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+class PostServiceTest {
+
+    @Autowired
+    private PostService postService;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @BeforeEach
+    public void clean() {
+        postRepository.deleteAllInBatch();
+    }
+
+    @DisplayName("글 작성")
+    @Test
+    void test1() {
+        // given
+        PostCreate postCreate = PostCreate.builder()
+                .title("제목")
+                .content("내용")
+                .build();
+
+        // when
+        postService.write(postCreate);
+
+        // then
+        assertThat(postRepository.count()).isEqualTo(1);
+
+        Post post = postRepository.findAll().get(0);
+        assertThat(post.getTitle()).isEqualTo("제목입니다.");
+        assertThat(post.getContent()).isEqualTo("내용입니다.");
+    }
+
+}
