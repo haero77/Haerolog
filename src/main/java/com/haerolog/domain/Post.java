73704314676a6b27a6a31b30c65b4c377ab777ba
haero77 <shineecard5@gmail.com>
2023-11-18 12:58:43 +0900

    Tip) 엔티티에 getter 를 만들 때는 서비스의 정책을 절대 넣지 마라

--- src/main/java/com/haerolog/domain/Post.java
+++ src/main/java/com/haerolog/domain/Post.java
@@ -27,4 +27,11 @@ public class Post {
         this.content = content;
     }
 
+    /**
+     * 엔티티에 getter 메서드를 만들 때는 서비스의 정책을 절대 넣지 마라. (다른 정책과 충돌 발생가능)
+     */
+//    public String getTitle() {
+//        return title.substring(0, 10);
+//    }
+
 }
