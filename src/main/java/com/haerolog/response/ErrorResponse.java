b1f346267222671b1a66db9c3d5896c0e7467f8e
haero77 <shineecard5@gmail.com>
2024-01-01 01:42:31 +0900

    refactor(ExceptionController): 팩토리 메서드로 에러 응답 객체 생성하도록 변경

--- src/main/java/com/haerolog/response/ErrorResponse.java
+++ src/main/java/com/haerolog/response/ErrorResponse.java
@@ -1,11 +1,11 @@
 package com.haerolog.response;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.validation.FieldError;
 
 /**
  *  {
@@ -21,16 +21,29 @@ public class ErrorResponse {
 
     private final String code;
     private final String message;
-    private final Map<String, String> validation = new HashMap<>();
+    private final Map<String, String> validation;
 
     @Builder
-    private ErrorResponse(String code, String message) {
+    private ErrorResponse(String code, String message, Map<String, String> validation) {
         this.code = code;
         this.message = message;
+        this.validation = validation;
     }
 
-    public void addValidation(String fieldName, String errorMessage) {
-        this.validation.put(fieldName, errorMessage);
+    public static ErrorResponse badRequest(List<FieldError> fieldErrors) {
+        return ErrorResponse.builder()
+                .code("400")
+                .message("잘못된 요청입니다.")
+                .validation(toValidation(fieldErrors))
+                .build();
+    }
+
+    private static Map<String, String> toValidation(List<FieldError> fieldErrors) {
+        return fieldErrors.stream()
+                .collect(Collectors.toMap(
+                        fieldError -> fieldError.getField(),
+                        fieldError -> fieldError.getDefaultMessage()
+                ));
     }
 
 }
