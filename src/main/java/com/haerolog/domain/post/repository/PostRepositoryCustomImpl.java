bb62b68af7cc15fcae81f6aafa02be8a8f077247
haero77 <shineecard5@gmail.com>
2024-01-27 05:20:15 +0900

    build: src/main/generated 를 source root 로 지정함으로써 QClass 인식되지 않는 문제 수정

--- src/main/java/com/haerolog/domain/post/repository/PostRepositoryCustomImpl.java
+++ src/main/java/com/haerolog/domain/post/repository/PostRepositoryCustomImpl.java
@@ -1,29 +1,31 @@
 package com.haerolog.domain.post.repository;
 
 
-import static com.haerolog.domain.post.application.domain.QPost.post;
-
 import com.haerolog.domain.post.application.domain.Post;
 import com.haerolog.domain.post.application.service.request.PostSearch;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static com.haerolog.domain.post.application.domain.QPost.post;
+
 
 @Repository
 @RequiredArgsConstructor
 public class PostRepositoryCustomImpl implements PostRepositoryCustom {
 
-    private final JPAQueryFactory queryFactory;
+	private final JPAQueryFactory queryFactory;
 
-    @Override
-    public List<Post> getList(PostSearch postSearch) {
-        return queryFactory
-                .selectFrom(post)
-                .limit(postSearch.getSize())
-                .offset(postSearch.getOffset())
-                .orderBy(post.id.desc())
-                .fetch();
-    }
+	@Override
+	public List<Post> getList(PostSearch postSearch) {
+		return queryFactory
+				.selectFrom(post)
+				.limit(postSearch.getSize())
+				.offset(postSearch.getOffset())
+				.orderBy(post.id.desc())
+				.fetch();
+	}
 
 }
