eb254194d3a454d9def4714e7aa24eafd22a1bd7
haero77 <shineecard5@gmail.com>
2024-04-03 22:54:42 +0900

    feat: 응답 헤더에 쿠키 싣기

--- src/main/java/com/haerolog/domain/auth/api/LoginApi.java
+++ src/main/java/com/haerolog/domain/auth/api/LoginApi.java
@@ -2,14 +2,19 @@ package com.haerolog.domain.auth.api;
 
 import com.haerolog.domain.auth.service.login.LoginRequest;
 import com.haerolog.domain.auth.service.login.LoginService;
-import com.haerolog.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.time.Duration;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class LoginApi {
@@ -20,12 +25,25 @@ public class LoginApi {
      * (호돌맨) auth는 보통 POST로 받음
      */
     @PostMapping("/api/v1/login")
-    public ApiResponse<SessionResponse> login(
+    public ResponseEntity<Object> login(
             @RequestBody @Valid LoginRequest loginRequest
     ) {
         String accessToken = loginService.login(loginRequest);
-        SessionResponse response = SessionResponse.fromAccessToken(accessToken);
-        return ApiResponse.ok(response);
+
+        ResponseCookie cookie = ResponseCookie.from("SESSION", accessToken)
+                .domain("localhost") // todo 서버 환경에 따른 분리 필요.
+                .path("/")
+                .httpOnly(true)
+                .secure(false)
+                .maxAge(Duration.ofDays(30))
+                .sameSite("Strict")
+                .build();
+
+        log.info(">>>>>>>> cookie={}", cookie.toString());
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .build();
     }
 
 }
