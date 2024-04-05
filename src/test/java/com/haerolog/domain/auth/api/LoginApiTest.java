8557765c5ffd6680027a86490b2534aedbe804e8
haero77 <shineecard5@gmail.com>
2024-04-05 22:13:24 +0900

    test: 쿠키 인증에 대한 테스트 케이스 추가

--- src/test/java/com/haerolog/domain/auth/api/LoginApiTest.java
+++ src/test/java/com/haerolog/domain/auth/api/LoginApiTest.java
@@ -6,6 +6,7 @@ import com.haerolog.domain.user.model.User;
 import com.haerolog.domain.user.repository.UserRepository;
 import com.haerolog.support.IntegrationTestSupport;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 class LoginApiTest extends IntegrationTestSupport {
 
@@ -31,6 +31,7 @@ class LoginApiTest extends IntegrationTestSupport {
         sessionRepository.deleteAllInBatch();
     }
 
+    @Disabled
     @DisplayName("로그인 성공 후 세션을 응답한다.")
     @Test
     void login() throws Exception {
@@ -57,6 +58,35 @@ class LoginApiTest extends IntegrationTestSupport {
                 .andExpect(jsonPath("$.status").value(200))
                 .andExpect(jsonPath("$.code").value("200"))
                 .andExpect(jsonPath("$.data.accessToken").exists())
+                .andDo(print());
+
+        int sessionSize = sessionRepository.findAll().size();
+        assertThat(sessionSize).isEqualTo(1);
+    }
+
+    @DisplayName("로그인 성공 후 세션이 생성되고, 응답 쿠키에 세션을 포함한다.")
+    @Test
+    void login_session() throws Exception {
+        // given
+        userRepository.save(
+                User.builder()
+                        .email("email@email.com")
+                        .password("password")
+                        .build()
+        );
+
+        LoginRequest loginRequest = LoginRequest.builder()
+                .email("email@email.com")
+                .password("password")
+                .build();
+
+        // expected
+        mockMvc.perform(post("/api/v1/login")
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loginRequest))
+                )
+                .andExpect(status().isOk())
+                .andExpect(cookie().exists("SESSION")) // https://www.tabnine.com/code/java/methods/org.springframework.test.web.servlet.result.MockMvcResultMatchers/cookie
                 .andDo(print());
 
         int sessionSize = sessionRepository.findAll().size();
