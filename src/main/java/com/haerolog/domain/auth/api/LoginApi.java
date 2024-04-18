7ff4667a360eadf9bb9594abe9582e81056b64d1
haero77 <shineecard5@gmail.com>
2024-04-18 21:51:17 +0900

    feat: (auth) JWT 로그인 - accessToken 발급 기능

--- src/main/java/com/haerolog/domain/auth/api/LoginApi.java
+++ src/main/java/com/haerolog/domain/auth/api/LoginApi.java
@@ -2,6 +2,7 @@ package com.haerolog.domain.auth.api;
 
 import com.haerolog.domain.auth.service.login.LoginRequest;
 import com.haerolog.domain.auth.service.login.LoginService;
+import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.crypto.SecretKey;
 import javax.validation.Valid;
 import java.time.Duration;
 
@@ -44,6 +46,30 @@ public class LoginApi {
 
         return ResponseEntity.ok()
                 .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .build();
+    }
+
+    @PostMapping("/api/v1/login/jwt")
+    public ResponseEntity<Object> loginJwt(
+    ) {
+        SecretKey secretKey = Jwts.SIG.HS256.key().build();
+
+        String jws = Jwts.builder() // Jwts(=JWT Signature)
+                .subject("Joe")
+                .signWith(secretKey)
+                .compact();
+
+        ResponseCookie responseCookie = ResponseCookie.from("accessToken", jws)
+                .domain("localhost") // todo 서버 환경에 따른 분리 필요.
+                .path("/")
+                .httpOnly(true)
+                .secure(false)
+                .maxAge(Duration.ofMinutes(10))
+                .sameSite("Strict")
+                .build();
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, responseCookie.toString())
                 .build();
     }
 
